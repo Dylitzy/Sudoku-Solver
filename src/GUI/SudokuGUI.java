98a8221d198a1c00dfeadeca0084993c31affefd
Dylan <dylanps4495@gmail.com>
2024-05-29 20:17:39 -0400

    - Sudoku Solver works! Not perfectly efficient, but definitely good enough.
    - Begun work on GUI, stubbed out necessary methods for now.

--- src/GUI/SudokuGUI.java
+++ src/GUI/SudokuGUI.java
@@ -1,9 +1,30 @@
 package GUI;
 
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.stage.Stage;
+
 /**
  * Represents the View and Controller of the Sudoku GUI.
  *
  * @author Dylan Sturr
  */
-public class SudokuGUI {
+public class SudokuGUI extends Application {
+    public SudokuGUI(){
+
+    }
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
+
+    }
 }
